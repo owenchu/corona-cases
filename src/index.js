2e13448dc4d28b5aa03b231b3526b8e1f6183f87
Owen Chu <owenchu@gmail.com>
2020-05-17 23:43:44 -0700

    Add google/firebase analytics.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import * as firebase from "firebase/app";
+import "firebase/analytics";
 import {
   ThemeProvider,
   createMuiTheme,
@@ -9,6 +11,20 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 import rawTheme from './theme.json';
+
+if (process.env.NODE_ENV === 'production') {
+  firebase.initializeApp({
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  });
+  firebase.analytics();
+}
 
 // https://colorhunt.co/palette/167377
 const theme = createMuiTheme(rawTheme);
